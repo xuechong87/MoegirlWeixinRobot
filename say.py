ff7ab184692efda30fa0dd4904b0bcb2513d8883
xuechong87 <xuechong87@gmail.com>
2013-05-30 22:54:06 +0800

    27

--- say.py
+++ say.py
@@ -1,8 +1,13 @@
+'''
+Created on 2013-5-30
+
+@author: xuechong
+'''
 import webapp2
 
-class HellowWorld(webapp2.RequestHandler):
+class Say(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.out.write('this is say')
 
-app = webapp2.WSGIApplication([('/', HellowWorld)])
+app = webapp2.WSGIApplication([('/say', Say)])
