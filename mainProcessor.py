6fd577196a79f3bdf387803b4a036c172f493688
xuechong <xuechong87@gmail.com>
2013-07-01 16:14:29 +0800

    vali fin

--- mainProcessor.py
+++ mainProcessor.py
@@ -11,24 +11,21 @@ weixin_token = "token"
 class MainProcessor(webapp2.RequestHandler):
     
     def get(self):
-        attr = self.request.__getattribute__()
-        signature = attr("signature")
-        timestamp = attr("timestamp")
-        nonce = attr("nonce")
-        echostr = attr("echostr")
+        param = self.request.get
+        write = self.response.out.write
         
-        list_ = sorted([weixin_token, timestamp, nonce])
+        list_ = sorted([weixin_token, param("timestamp"), param("nonce")])
         sha1 = hashlib.sha1()
         sha1.update("".join(list_))
-        
+
         self.response.headers['Content-Type'] = 'text/plain'
-        write = self.response.out.write()
-        if sha1.digest() == signature:
-            write(echostr)
+        
+        if sha1.digest() == param("signature"):
+            write(param("echostr"))
         else:
-            write("error")
+            write("what's up man -.-?")
         
     def post(self):
-        return
+        return self.get(self)
 
-app = webapp2.WSGIApplication([('/', MainProcessor)])
+app = webapp2.WSGIApplication([('/weixin', MainProcessor)])
