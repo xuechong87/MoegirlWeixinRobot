bc830dd73b40d37322d11fa83ef304625837d679
xuechong87 <xuechong87@gmail.com>
2013-07-02 21:13:19 +0800

    nothing

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import webapp2
 class Main(webapp2.RequestHandler):
     
     def get(self):
-        self.post(self)
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write('this is say')
         
     def post(self):
         self.response.headers['Content-Type'] = 'text/plain'
