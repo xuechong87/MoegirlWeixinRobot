a5c15b66c69495db3c5b33f32063559dfc26bd39
xuechong87 <xuechong87@gmail.com>
2013-07-07 23:15:00 +0800

    memcache fin

--- moehandlers/CoderHandler.py
+++ moehandlers/CoderHandler.py
@@ -1,34 +1,55 @@
 # coding: utf-8
 '''
 Created on 2013-7-7
-
+the coder lao huang li!!! for moegirl wiki
 @author: xuechong
 '''
 from google.appengine.api import memcache
+from Weixin import textReply
+import time
 import logging
+from utils.Commons import randomFromList
 
 client = memcache.Client()
 __coderCalenderKey__ = ""
+__namespace__ = "coderCalender"
 
-class CoderCal():
+class CoderCalender():
+    """
+    the coder lao huang li!!! for moegirl wiki
+    """
     @staticmethod
     def __helpkey__ ():
         return __coderCalenderKey__
     @staticmethod
     def __helpcontent__():
         return ""
-    def handle(self,handlerChain):
-        pass
     
-def todayKey():
-    return ""
+    def handle(self,handlerChain):
+        if handlerChain.getMsgContent() == __coderCalenderKey__:
+            return textReply(handlerChain.userMsg,find())
+        else:
+            return handlerChain.invokeNext()
+    
+todayKey = lambda : "coderCalender" + time.strftime('%Y-%m-%d',time.localtime(time.time()))
 
 def find():
-    result =client.get(todayKey(), "coderCalender")
+    result =client.get(todayKey(), __namespace__)
     if result is None:
         result = addNew()
-
+    return result
 
 def addNew():
     logging.info("create new coder calender")
-    
+    content = newContent()
+    client.set(key=todayKey(),\
+               value = content,\
+               time=24*60*60,\
+               namespace=__namespace__)
+    return content
+
+def newContent():
+    return ""
+
+if __name__ == "__main__":
+    print todayKey()
---
+++ utils/Commons.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+'''
+Created on 2013-7-7
+
+@author: xuechong
+'''
+import random
+import logging
+
+randomFromList = lambda list_:list_[random.randint(0,len(list_)-1)]
