52dabd74367abe968d91c556637b307ceece5a5a
xuechong87 <xuechong87@gmail.com>
2013-08-05 23:07:13 +0800

    modify read me

--- moehandlers/SellMoeHandler.py
+++ moehandlers/SellMoeHandler.py
@@ -10,6 +10,7 @@ import logging
 
 sellMoeList=["自动应答姬卖萌中~\(≧▽≦)/~啦啦啦","人家还只会卖萌了啦 (＞﹏＜)","自动应答姬努力卖萌中(oﾟωﾟo)","自动应答姬各种卖萌中(＞。☆)"]
 keyWordDict = {"么么哒":"欧尼酱么么哒\(≧ω≦)/","无路赛":"夏娜酱(＞﹏＜)!","给大爷笑一个":"讨厌了啦,就会欺负人家"}
+helpmsg = "\n\n\n☆更新姬提示☆\n输入'帮助'可以查看现有功能哦"
 class SellMoeHandler(object):
     @staticmethod
     def __helpkey__ ():
@@ -24,9 +25,9 @@ class SellMoeHandler(object):
         if(handlerChain.getMsgType()!="text"):
             return None#if you cant handle this msg ,just return None or return handlerChain.invokeNext()
         if keyWordDict.has_key(handlerChain.getMsgContent()):
-            return Weixin.textReply(handlerChain.userMsg,keyWordDict.get(handlerChain.getMsgContent()))
+            return Weixin.textReply(handlerChain.userMsg,keyWordDict.get(handlerChain.getMsgContent()) + helpmsg)
         answer = random.randint(0,len(sellMoeList)-1)
-        return Weixin.textReply(handlerChain.userMsg,sellMoeList[answer])
+        return Weixin.textReply(handlerChain.userMsg,sellMoeList[answer]+helpmsg)
     
     
     
