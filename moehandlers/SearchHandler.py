e6624d7b8e4503d1ba7c8f362231fdec7d4cee77
xuechong87 <xuechong87@gmail.com>
2013-08-07 22:47:22 +0800

    search half

--- moehandlers/SearchHandler.py
+++ moehandlers/SearchHandler.py
@@ -1,9 +1,13 @@
+# coding: utf-8
 '''
 Created on 2013-8-7
 search the moegirl wiki
 @author: xuechong
 '''
+import MoeGirlWiki
+import Weixin
 
+__suf__="是什么"
 
 class SearchHandler():
     @staticmethod
@@ -12,5 +16,24 @@ class SearchHandler():
     @staticmethod
     def __helpcontent__():
         return ""
+    
+    def handle(self,handlerChain):
+        
+        if (handlerChain.getMsgType()=="text"):
+            content = handlerChain.getMsgContent()
+            if hasKeyWord(content):
+                searchKey = content[:-3]
+                resultList = MoeGirlWiki.searchTitle(searchKey)
+                return Weixin.textReply(handlerChain.userMsg,buildReplyStr(resultList))
+        return handlerChain.invokeNext()
+    
+def hasKeyWord(content):
+    return (content !="" and content is not None) and content.endswith(__suf__)
+    
+
+def buildReplyStr(wikiList):
+    for subject in  wikiList:
+        pass
+        
     
     
